34455645ac6e2fec325661860edf3b6b0e2d08e7
vuongdinhhieu2003 <vuongdinhhieu2003@gmail.com>
2022-01-03 20:48:22 +0700

    Hoan thanh lan 1

--- JS/length.js
+++ JS/length.js
@@ -11,14 +11,14 @@ function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
 
-  fetch(`https://v6.exchangerate-api.com/v6/b71ba5a6f69833fe3ac900ec/latest/${currency_one}`)
+  fetch(`API/api-Length/${currency_one}.json`)
     .then((res) => res.json())
     .then((data) => {
       //   console.log(data);
       const rate = data.conversion_rates[currency_two];
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
 
-      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(4);
     });
 }
 
